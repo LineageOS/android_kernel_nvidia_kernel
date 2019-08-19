4b9fde0f5392283415fd8895332e00f0e436b2e1
David Dai <daidavid1@codeaurora.org>
2019-08-19 16:35:44 -0700

    ANDROID: clk: add pre and post change rate callbacks
    There are scenarios where a rate change could result in a configuration
    change for both the targeted clock and its parent.
    
    For example, setting the rate for a clock could require both slewing its parent
    PLL as well as adjusting the clock's divider values. Due to the fact that
    rate change propagation always occurs from parent to child, we could exceed
    the allowed operating frequencies for the clock as the parent slews to a higher
    frequency before increasing the downstream divider.
    
    Add a pre change call back which allows the clock to adjust its divider
    appropriately before any rate change has occurred from its parents to ensure
    that the clock's requirements are always within safe frequencies during parent
    rate changes. The symmetrical post change call back handles the scenario where
    the divider adjusts to a lower value and can only be safely adjusted after the
    parent rate changes.
    
    Change-Id: I4f8cf9df6fc256d065599de86a34cf99eae4d853
    Signed-off-by: David Dai <daidavid1@codeaurora.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -2296,6 +2296,13 @@ static struct clk_core *clk_propagate_rate_change(struct clk_core *core,
 			fail_clk = core;
 	}
 
+	if (core->ops->pre_rate_change) {
+		ret = core->ops->pre_rate_change(core->hw, core->rate,
+						 core->new_rate);
+		if (ret)
+			fail_clk = core;
+	}
+
 	hlist_for_each_entry(child, &core->children, child_node) {
 		/* Skip children who will be reparented to another clock */
 		if (child->new_parent && child->new_parent != core)
@@ -2423,6 +2430,9 @@ static void clk_change_rate(struct clk_core *core)
 
 	if (core->flags & CLK_RECALC_NEW_RATES)
 		(void)clk_calc_new_rates(core, core->new_rate);
+
+	if (core->ops->post_rate_change)
+		core->ops->post_rate_change(core->hw, old_rate, core->rate);
 
 	/*
 	 * Allow children to be aware that next set rate operation is triggered
