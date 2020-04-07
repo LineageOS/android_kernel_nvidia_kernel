273ba67d3b7d622a63fc2c91e6cb97d13cefe0ab
Kelly Rossmoyer <krossmo@google.com>
2020-04-07 12:25:33 -0700

    ANDROID: power: wakeup_reason: wake reason enhancements
    These changes build upon the existing Android kernel wakeup reason code
    to:
    * improve the positioning of suspend abort logging calls in suspend flow
    * add logging of abnormal wakeup reasons like unexpected HW IRQs and
      IRQs configured as both wake-enabled and no-suspend
    * add support for capturing deferred-processing threaded nested IRQs as
      wakeup reasons rather than their synchronously-processed parents
    
    Bug: 150970830
    Bug: 140217217
    Bug: 120445600
    
    Signed-off-by: Kelly Rossmoyer <krossmo@google.com>
    Change-Id: I903b811a0fe11a605a25815c3a341668a23de700
    [ebiggers: folded commit f46fe356eb44 ("ANDROID: fix wakeup reason
               findings") into this patch]
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -15,7 +15,9 @@
 #include <linux/seq_file.h>
 #include <linux/debugfs.h>
 #include <linux/pm_wakeirq.h>
-#include <linux/types.h>
+#include <linux/irq.h>
+#include <linux/irqdesc.h>
+#include <linux/wakeup_reason.h>
 #include <trace/events/power.h>
 
 #include "power.h"
@@ -943,6 +945,7 @@ bool pm_wakeup_pending(void)
 {
 	unsigned long flags;
 	bool ret = false;
+	char suspend_abort[MAX_SUSPEND_ABORT_LEN];
 
 	raw_spin_lock_irqsave(&events_lock, flags);
 	if (events_check_enabled) {
@@ -957,6 +960,10 @@ bool pm_wakeup_pending(void)
 	if (ret) {
 		pm_pr_dbg("Wakeup pending, aborting suspend\n");
 		pm_print_active_wakeup_sources();
+		pm_get_active_wakeup_sources(suspend_abort,
+					     MAX_SUSPEND_ABORT_LEN);
+		log_suspend_abort_reason(suspend_abort);
+		pr_info("PM: %s\n", suspend_abort);
 	}
 
 	return ret || atomic_read(&pm_abort_suspend) > 0;
@@ -1006,8 +1013,20 @@ void pm_system_irq_wakeup(unsigned int irq_number)
 
 	raw_spin_unlock_irqrestore(&wakeup_irq_lock, flags);
 
-	if (irq_number)
+	if (irq_number) {
+		struct irq_desc *desc;
+		const char *name = "null";
+
+		desc = irq_to_desc(irq_number);
+		if (desc == NULL)
+			name = "stray irq";
+		else if (desc->action && desc->action->name)
+			name = desc->action->name;
+
+		log_irq_wakeup_reason(irq_number);
+		pr_warn("%s: %d triggered %s\n", __func__, irq_number, name);
 		pm_system_wakeup();
+	}
 }
 
 unsigned int pm_wakeup_irq(void)
