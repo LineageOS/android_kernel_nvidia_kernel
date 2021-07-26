4eb4c018ac760dbb60d75e5c3d86e8741e35e958
Hridya Valsaraju <hridya@google.com>
2021-07-25 20:49:06 -0700

    ANDROID: staging: ion: move buffer kmap from begin/end_cpu_access()
    Since dma_buf_begin/end_cpu_access() calls always used to bracket
    dma_buf_kmap/kunmap calls, ION performed kmap/kunmap invocations for the
    buffer during dma_buf_begin/end_cpu_access() calls and cached the
    results with a kmap counter.
    However, dma_buf_begin/end_cpu_access() invocations can be
    triggered from the userspace using the DMA_BUF_IOC_SYNC ioctl as well.
    This means that a mapping that was created by a device driver using by a
    dma_buf_kmap() call or an ion_map_kernel() call could be unmapped
    from userspace if a client accidentally(or maliciously) invoked
    DMA_BUF_IOCTL_SYNC IOCTL with 'DMA_BUF_SYNC_END' argument since this
    would inturn invoke dma_buf_end_cpu_access() which would then decrement
    the kmap counter and invoke kunmap() when the counter gets to 0.
    
    This patch moves the kmap/kunmap operations from the
    begin/end_cpu_access() DMA-BUF ops to the map/unmap DMA-BUF ops to
    prevent the issue.
    
    Bug: 187527909
    Change-Id: I00dc8eefefb1f3aab99e770f90d624011f7740f0
    [hridya: minor conflicts during cherry-picking]
    Signed-off-by: Hridya Valsaraju <hridya@google.com>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -1025,11 +1025,6 @@ static void ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf, size_t start,
 				       size_t len,
 				       enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
-
-	mutex_lock(&buffer->lock);
-	ion_buffer_kmap_put(buffer);
-	mutex_unlock(&buffer->lock);
 }
 
 static struct dma_buf_ops dma_buf_ops = {
