d5addb990e49188ed768c90a4f6560569dd95d13
Henrik Grimler <henrik@grimler.se>
2021-09-29 20:14:18 +0200

    power: supply: max17042_battery: fix typo in MAX17042_IAvg_empty
    Datasheet gives the name IAvg_empty, not LAvg_empty.
    
    Signed-off-by: Henrik Grimler <henrik@grimler.se>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Change-Id: Ib2a123a2bd0ed557cfca6614f16e89257702da69

--- drivers/power/supply/max17042_battery.c
+++ drivers/power/supply/max17042_battery.c
@@ -785,7 +785,7 @@ static inline void max17042_override_por_values(struct max17042_chip *chip)
 	if (chip->chip_type == MAXIM_DEVICE_TYPE_MAX17042)
 		max17042_override_por(map, MAX17042_SOC_empty,
 						config->socempty);
-	max17042_override_por(map, MAX17042_LAvg_empty, config->lavg_empty);
+	max17042_override_por(map, MAX17042_IAvg_empty, config->iavg_empty);
 	max17042_override_por(map, MAX17042_dQacc, config->dqacc);
 	max17042_override_por(map, MAX17042_dPacc, config->dpacc);
 
