CodeLinaro-mirror/kernel-platform-2-0-os_kernel_common

ANDROID: drivers: gpu: drm: export drm_mode_convert_umode symbol
Export drm_mode_convert_umode symbol for module consumption.

Bug: 139653858
Signed-off-by: Shashank Babu Chinta Venkata <sbchin@codeaurora.org>

drivers/gpu/drm/drm_modes.c
@@ -2045,6 +2045,7 @@ int drm_mode_convert_umode(struct drm_device *dev,
 
 	return 0;
 }
+EXPORT_SYMBOL(drm_mode_convert_umode);
 
 /**
  * drm_mode_is_420_only - if a given videomode can be only supported in YCBCR420

LineageOS/android_kernel_nvidia_kernel

Change-Id: I5d4c15a5edb28616a2ff01d5acdf498bad6973d8

drivers/gpu/drm/drm_modes.c
@@ -2016,6 +2016,7 @@ int drm_mode_convert_umode(struct drm_device *dev,
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(drm_mode_convert_umode);
 
 /**
  * drm_mode_is_420_only - if a given videomode can be only supported in YCBCR420