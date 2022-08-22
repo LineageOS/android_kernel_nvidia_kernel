FlopKernel-Series/flop_s5e8825_kernel

ANDROID: mm/memory.c: export mm_trace_rss_stat
to workaround following error:
    ERROR: "mm_trace_rss_stat" [drivers/gpu/arm_gpu/mali_kbase.ko] undefined!
reported when make modules

Test: test 5.5.0-rc1-mainline with hikey960 with GKI method

Change-Id: Icb56b0c760780c3f9081f82d921406d17f4f1c08
[adelva: workaround for Mali driver on hikey960]
Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
Signed-off-by: Alistair Delva <adelva@google.com>

mm/memory.c
@@ -158,6 +158,7 @@ void mm_trace_rss_stat(struct mm_struct *mm, int member, long count)
 {
 	trace_rss_stat(mm, member, count);
 }
+EXPORT_SYMBOL(mm_trace_rss_stat);
 
 #if defined(SPLIT_RSS_COUNTING)
 

LineageOS/android_kernel_nvidia_kernel

mm/memory.c
@@ -160,6 +160,7 @@ void mm_trace_rss_stat(struct mm_struct *mm, int member, long count)
 {
 	trace_rss_stat(mm, member, count);
 }
+EXPORT_SYMBOL_GPL(mm_trace_rss_stat);
 
 #if defined(SPLIT_RSS_COUNTING)
 