123c23a603c2b1ddb9ea83f8772b09e3e4ec6c9c
Diogo Ivo <diogo.ivo@tecnico.ulisboa.pt>
2025-06-30 09:48:42 +0100

    drm/tegra: Add NVJPG driver
    Add support for booting and using NVJPG on Tegra210 to the Host1x
    and TegraDRM drivers. This driver only supports the new TegraDRM uAPI.
    
    Acked-by: Mikko Perttunen <mperttunen@nvidia.com>
    Change-Id: Ibbd11dc9d03e0f284528070b4ff305443bb7bf0b
    Signed-off-by: Diogo Ivo <diogo.ivo@tecnico.ulisboa.pt>

--- drivers/gpu/drm/tegra/drm.c
+++ drivers/gpu/drm/tegra/drm.c
@@ -1383,6 +1383,7 @@ static const struct of_device_id host1x_drm_subdevs[] = {
 	{ .compatible = "nvidia,tegra210-sor1", },
 	{ .compatible = "nvidia,tegra210-vic", },
 	{ .compatible = "nvidia,tegra210-nvdec", },
+	{ .compatible = "nvidia,tegra210-nvjpg", },
 	{ .compatible = "nvidia,tegra186-display", },
 	{ .compatible = "nvidia,tegra186-dc", },
 	{ .compatible = "nvidia,tegra186-sor", },
@@ -1421,6 +1422,7 @@ static struct platform_driver * const drivers[] = {
 	&tegra_gr3d_driver,
 	&tegra_vic_driver,
 	&tegra_nvdec_driver,
+	&tegra_nvjpg_driver,
 };
 
 static int __init host1x_drm_init(void)
