e31c6bb1beab7a6913aa7bfc7e9a69df850238d5
Aaron Kling <webgeek1234@gmail.com>
2025-12-18 15:36:08 -0600

    cpufreq: Add Tegra186 and Tegra194 to cpufreq-dt-platdev blocklist
    These have platform specific drivers.
    
    Change-Id: If69113850a1af5bae5ae9d68d5b69db1f71857ac
    Signed-off-by: Aaron Kling <webgeek1234@gmail.com>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -142,6 +142,8 @@ static const struct of_device_id blocklist[] __initconst = {
 	{ .compatible = "nvidia,tegra20", },
 	{ .compatible = "nvidia,tegra30", },
 	{ .compatible = "nvidia,tegra124", },
+	{ .compatible = "nvidia,tegra186", },
+	{ .compatible = "nvidia,tegra194", },
 	{ .compatible = "nvidia,tegra210", },
 	{ .compatible = "nvidia,tegra234", },
 
